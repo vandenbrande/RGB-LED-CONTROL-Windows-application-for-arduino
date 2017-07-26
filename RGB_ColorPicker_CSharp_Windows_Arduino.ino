e79d5b4118675101bb415cfeb9167a9217e37653
Danny van den Brande <29558616+vandenbrande@users.noreply.github.com>
2017-07-26 02:02:23 +0200

    ARDUINO CODE

---
+++ RGB_ColorPicker_CSharp_Windows_Arduino.ino
@@ -0,0 +1,20 @@
+const int RED_LED_PIN = 5;
+const int GREEN_LED_PIN = 9;
+const int BLUE_LED_PIN = 11;
+
+void setup() { 
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  
+  if (Serial.available() == 3)
+  {
+    analogWrite(RED_LED_PIN, Serial.read());
+    analogWrite(GREEN_LED_PIN, Serial.read());
+    analogWrite(BLUE_LED_PIN, Serial.read());
+   
+   
+  }
+}
